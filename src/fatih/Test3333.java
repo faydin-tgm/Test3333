c6896e2c2adfba70b95b17a1c848a9b9a9582b70
Faydin <faydin@student.tgm.ac.at>
2017-03-16 10:56:14 +0100

    V00.01.K01

--- src/fatih/Test3333.java
+++ src/fatih/Test3333.java
@@ -3,7 +3,7 @@ package fatih;
 public class Test3333 {
 	
 	public static void main(String[] args){
-		System.out.println("Guten Morgen Herr Aydin");
+		System.out.println("Guten Morgen Herr Aydin :)");
 		
 	}
 }
